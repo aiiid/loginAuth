693890d3ade276177e90478f58ec05f2fd7e796d
Aida <ai.abdirakhman@gmail.com>
2024-07-08 18:34:53 +0500

    Eye icon fixed

--- Login Auth training/LoginScreen/Custom/CustomTextField.swift
+++ Login Auth training/LoginScreen/Custom/CustomTextField.swift
@@ -25,37 +25,57 @@ class CustomTextField: UITextField {
     }
     
     private func configureTextField(isPasswordField: Bool) {
-            let placeholderAttributes: [NSAttributedString.Key: Any] = [
-                .foregroundColor: UIColor.lightGray
-            ]
-            attributedPlaceholder = NSAttributedString(string: placeholder ?? "", attributes: placeholderAttributes)
+        let placeholderAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.lightGray
+        ]
+        attributedPlaceholder = NSAttributedString(string: placeholder ?? "", attributes: placeholderAttributes)
+        
+        borderStyle = .none
+        backgroundColor = UIColor.systemGray6
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
+        
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        leftView = paddingView
+        leftViewMode = .always
+        
+        if isPasswordField {
+            eyeButton.setImage(UIImage(systemName: "eye.fill"), for: .normal)
+            eyeButton.setImage(UIImage(systemName: "eye.slash.fill"), for: .selected)
+            eyeButton.tintColor = Constants.Colors.gray
+            eyeButton.addTarget(self, action: #selector(togglePasswordVisibility), for: .touchUpInside)
             
-            borderStyle = .none
-            backgroundColor = UIColor.systemGray6
-            layer.cornerRadius = 10
-            layer.masksToBounds = true
+            let rightPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10)) // Smaller width for padding
+            let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 40)) // Adjusted width for container view
             
-            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 40))
-            leftView = paddingView
-            leftViewMode = .always
+            containerView.addSubview(rightPaddingView)
+            containerView.addSubview(eyeButton)
             
-            if isPasswordField {
-                eyeButton.setImage(UIImage(systemName: "eye.fill"), for: .normal)
-                eyeButton.setImage(UIImage(systemName: "eye.slash.fill"), for: .selected)
-                eyeButton.addTarget(self, action: #selector(togglePasswordVisibility), for: .touchUpInside)
-                rightView = eyeButton
-                rightViewMode = .always
+            rightPaddingView.snp.makeConstraints { make in
+                make.leading.centerY.equalToSuperview()
+                make.width.equalTo(10) // Smaller width for padding
+                make.height.equalToSuperview()
             }
+            
+            eyeButton.snp.makeConstraints { make in
+                make.leading.equalTo(rightPaddingView.snp.trailing)
+                make.trailing.equalToSuperview().inset(10) // Adding inset for spacing
+                make.centerY.equalToSuperview()
+            }
+            
+            rightView = containerView
+            rightViewMode = .always
         }
+    }
     
     private func disableSmartFeatures() {
-            autocorrectionType = .no
-            spellCheckingType = .no
-            smartQuotesType = .no
-            smartDashesType = .no
-            smartInsertDeleteType = .no
-            autocapitalizationType = .none
-        }
+        autocorrectionType = .no
+        spellCheckingType = .no
+        smartQuotesType = .no
+        smartDashesType = .no
+        smartInsertDeleteType = .no
+        autocapitalizationType = .none
+    }
     
     @objc private func togglePasswordVisibility() {
         isSecureTextEntry.toggle()
--- Login Auth training/SplashScreen/SplashView.swift
+++ Login Auth training/SplashScreen/SplashView.swift
@@ -15,6 +15,22 @@ class SplashView: UIView {
         return image
     }()
     
+    let mainText: UILabel = {
+        var text = UILabel()
+        text.text = "Lorby"
+        text.font = Constants.Fonts.titleFont
+        text.textAlignment = .center
+        return text
+    }()
+    
+    let secondaryText: UILabel = {
+        var text = UILabel()
+        text.text = "Твой личный репетитор"
+        text.font = Constants.Fonts.bodyFont
+        text.textAlignment = .center
+        return text
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -27,9 +43,24 @@ class SplashView: UIView {
     private func setupView() {
         backgroundColor = .white
         addSubview(mainImage)
+        addSubview(mainText)
+        addSubview(secondaryText)
         
         mainImage.snp.makeConstraints { make in
             make.center.equalToSuperview()
         }
+        
+        mainText.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(safeAreaLayoutGuide).offset(118)
+            make.leading.trailing.equalToSuperview().inset(51)
+        }
+        
+        secondaryText.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(mainText.snp.bottom).offset(8)
+            make.leading.trailing.equalToSuperview().inset(51)
+        }
+        
     }
 }
